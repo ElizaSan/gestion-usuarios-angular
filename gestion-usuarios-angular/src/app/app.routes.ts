68d3c1b82142a365b65e5178e531b7f7c8c13844
Miri <paletaleblue@outlook.com>
2025-08-30 19:40:07 -0600

    Parte 4. Uso de routes, se usa app.routes.ts como archivo para manejo de rutas

--- gestion-usuarios-angular/src/app/app.routes.ts
+++ gestion-usuarios-angular/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Documentacion } from './pages/documentacion/documentacion';
+import { Estructuras } from './pages/estructuras/estructuras';
+import { Perfil } from './pages/perfil/perfil';
+import { Usuarios } from './pages/usuarios/usuarios';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'documentacion',
+    component: Documentacion
+  },
+  {
+    path: 'estructuras',
+    component: Estructuras
+  },
+  {
+    path: 'perfil',
+    component: Perfil
+  },
+  {
+    path: 'usuarios',
+    component: Usuarios
+  }
+
+];
